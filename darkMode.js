ec3d3f2d7c34a1dfbed4f140b659ea7d831d7d82
Nicola <nickbusato0101@gmail.com>
2024-02-07 11:13:28 +0100

    Update darkMode

--- darkMode.js
+++ darkMode.js
@@ -1,10 +1,14 @@
-if (loadThemeState() === null && window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+if (
+  loadThemeState() === null &&
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
   console.log("dark-mode: auto detect");
   console.log("aaaaa");
   console.log(loadThemeState());
 
   saveThemeState("dark");
-  applyTheme()
+  applyTheme();
 }
 
 function saveThemeState(theme) {
@@ -17,22 +21,29 @@ function loadThemeState() {
 
 window.addEventListener("load", applyTheme);
 
-const toggleDarkModeButton = document.getElementById("toggleDarkMode");
-toggleDarkModeButton.addEventListener("click", darkMode);
-
 function applyTheme() {
   if (loadThemeState() === "dark") {
     document.body.classList.add("dark-mode");
     console.log("dark-mode");
-  }
-  else {
+  } else {
     document.body.classList.remove("dark-mode");
     console.log("light-mode");
   }
 }
 
+const toggleDarkModeButton = document.getElementById("toggleDarkMode");
+toggleDarkModeButton.addEventListener("click", darkMode);
+
 function darkMode() {
+  console.log("toggleDarkModeButton");
+  const body = document.body;
+  body.classList.add("changing-theme");
+
   const isDarkMode = document.body.classList.contains("dark-mode");
   saveThemeState(isDarkMode ? "light" : "dark");
   applyTheme();
+
+  setTimeout(() => {
+    body.classList.remove("changing-theme");
+  }, 1000);
 }
