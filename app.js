f0cf5f12fff9fb1048a14c54dc0e77fbe8b5d82e
Michele Gusella <mgusella2@icloud.com>
2024-05-02 11:44:53 +0200

    added comments, fixed indentation

--- app.js
+++ app.js
@@ -1,193 +1,204 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const questionContainer = document.getElementById("question-container");
-  const calculateButton = document.getElementById("calculate-button");
-  const totalQuestionsContainer = document.getElementById("total-questions");
-  const showAllButton = document.getElementById("show-all-questions-button");
+document.addEventListener("DOMContentLoaded", function ()
+{
+	const questionContainer = document.getElementById("question-container");
+	const calculateButton = document.getElementById("calculate-button");
+	const totalQuestionsContainer = document.getElementById("total-questions");
+	const showAllButton = document.getElementById("show-all-questions-button");
 
-  let questions;
-  let userAnswers = [];
-  let randomQuestions;
-  let questionsToDisplay;
+	let questions; // array that contains all the questions ordered as in the input JSON file
+	let userAnswers = [];
+	let randomQuestions;
+	let questionsToDisplay;
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const currentPage = urlParams.get("page");
+	// get the name of the file to open
+	const urlParams = new URLSearchParams(window.location.search);
+	const currentPage = urlParams.get("page");
+	const questionsPath = `questions/questions_${currentPage}.json`;
 
-  // Determina il percorso del file questions.json in base alla pagina corrente
-  const questionsPath = `questions/questions_${currentPage}.json`;
+	// Fetch questions from the JSON file
+	fetch(questionsPath)
+	.then((response) => response.json())
+	.then((data) => {
+		questions = data;
+		console.log("Questions loaded:", questions);
+		totalQuestionsContainer.textContent = `Total Questions found on database: ${questions.length}`;
+		showQuestions();
+	})
+	.catch((error) => console.error("Error loading questions:", error));
 
-  // Fetch questions from the JSON file
-  fetch(questionsPath)
-    .then((response) => response.json())
-    .then((data) => {
-      questions = data;
-      console.log("Questions loaded:", questions);
-      totalQuestionsContainer.textContent = `Total Questions found on database: ${questions.length}`;
-      showQuestions();
-    })
-    .catch((error) => console.error("Error loading questions:", error));
+	// Event listener for the Calculate Score button and show all questions
+	calculateButton.addEventListener("click", calculateScore);
+	showAllButton.addEventListener("click", showAllQuestions);
 
-  // Event listener for the Calculate Score button and show all questions
-  calculateButton.addEventListener("click", calculateScore);
-  showAllButton.addEventListener("click", showAllQuestions);
+	// function to shuffle the questions and for each question, the answers
+	function shuffleArray(array)
+	{
+		for (let i = array.length - 1; i > 0; i--)
+		{
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+		return array;
+	}
 
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
+	// show given questions. If selectCorrectAnswer = true, check correct answers when printing them
+	function showQuestions(questionsToShow, selectCorrectAnswer = false)
+	{
+		console.log("Showing questions:");
 
-  // Aggiorna questa parte nel tuo file JavaScript
-  function showQuestions(questionsToShow, selectCorrectAnswer = false) {
-    console.log("Showing questions:");
+		// number of questions to show
+		numDom = 20;
 
-    numDom = 20;
-    // randomizzo le domande e ne prendo numDom
-    shuffledArray = questions.concat();
-    shuffledArray = shuffleArray(shuffledArray);
-    randomQuestions = shuffledArray.slice(0, numDom);
-    questionsToDisplay = questionsToShow || randomQuestions;
+		// shuffling questions and taking numDom of them
+		shuffledArray = questions.concat();
+		shuffledArray = shuffleArray(shuffledArray);
+		randomQuestions = shuffledArray.slice(0, numDom);
+		// if questionToShow is not null ==> questionsToDisplay = questionsToShow
+		// otherwise questionsToDisplay = randomQuestions (almost always the case, except when showAllQuestions is called)
+		questionsToDisplay = questionsToShow || randomQuestions;
 
-    // Loop through all questions
-    for (let i = 0; i < questionsToDisplay.length; i++) {
-      const currentQuestion = questionsToDisplay[i];
-      //const currentQuestion = questions[i];
-      const correctAnswerIndex = currentQuestion.correctAnswer;
+		// Loop through all questions to display
+		for (let i = 0; i < questionsToDisplay.length; i++)
+		{
+			const currentQuestion = questionsToDisplay[i];
+			const correctAnswerIndex = currentQuestion.correctAnswer;
 
-      // Mantieni traccia dell'indice originale prima di mescolare
-      const optionsWithIndices = currentQuestion.options.map(
-        (option, index) => ({
-          value: option,
-          index: index,
-        })
-      );
+			// Before shuffling the answers, save the correctAnswerIndex
+			const optionsWithIndices = currentQuestion.options.map(
+				(option, index) => ({
+					value: option,
+					index: index,
+				})
+			);
 
-      // Shuffle the array of options
-      const shuffledOptions = shuffleArray(optionsWithIndices);
+			// Shuffle the array of options (answers)
+			const shuffledOptions = shuffleArray(optionsWithIndices);
 
-      // Find the index of the correct answer in the shuffled array
-      //const correctAnswerIndex = currentQuestion.options.indexOf(currentQuestion.options[currentQuestion.correctAnswer]);
+			// Find the index of the correct answer in the shuffled array
+			//const correctAnswerIndex = currentQuestion.options.indexOf(currentQuestion.options[currentQuestion.correctAnswer]);
 
-      const optionsHTML = shuffledOptions
-        .map(
-          (option, index) => `
+			const optionsHTML = shuffledOptions.map((option, index) => `
 				<input type="radio" name="answer${i}" value="${option.index}" ${
-            selectCorrectAnswer && option.index === correctAnswerIndex
-              ? "checked"
-              : ""
-          }>
+				selectCorrectAnswer && option.index === correctAnswerIndex? "checked": ""}>
 				<label>${option.value}</label><br>
-			`
-        )
-        .join("");
+			`).join("");
 
-      const imageHTML = currentQuestion.image
-        ? `<img src="${currentQuestion.image}" alt="Question Image" class="question-image">`
-        : "";
+			const imageHTML = currentQuestion.image? 
+			`<img src="${currentQuestion.image}" alt="Question Image" class="question-image">`
+			: "";
 
-      console.log(`Question ${i + 1}:`, currentQuestion.question);
+			console.log(`Question ${i + 1}:`, currentQuestion.question);
 
-      // Aggiungi il numero della domanda all'inizio della domanda
-      const questionNumberHTML = `<div class="question-number">${i + 1}</div>`;
+			// Add index of question before showing it
+			const questionNumberHTML = `<div class="question-number">${i + 1}</div>`;
 
-      questionContainer.innerHTML += `
-				<div class="question" id="question${i}" data-verified="${currentQuestion.verified}">
-					${questionNumberHTML}
-					<div class="verification-icon"></div>
-					<h2>${currentQuestion.question}</h2>
-					${imageHTML}
-					${optionsHTML}
-					<div class="feedback"></div>
-				</div>
+			questionContainer.innerHTML += `
+			<div class="question" id="question${i}" data-verified="${currentQuestion.verified}">
+			${questionNumberHTML}
+			<div class="verification-icon"></div>
+			<h2>${currentQuestion.question}</h2>
+			${imageHTML}
+			${optionsHTML}
+			<div class="feedback"></div>
+			</div>
 			`;
-    }
+		}
 
-    console.log("All questions displayed.");
-  }
+		console.log("All questions displayed.");
+	}
 
-  // Modifica la funzione calculateScore nel tuo file JavaScript
-  function calculateScore() {
-    console.log("Calculating score:");
+	// Compute the score
+	function calculateScore()
+	{
+		console.log("Calculating score:");
 
-    // Reset user answers and feedback
-    userAnswers = [];
-    document.querySelectorAll(".feedback").forEach((feedback) => {
-      feedback.innerHTML = "";
-      feedback.classList.remove("correct", "incorrect");
-    });
+		// Reset user answers and feedback
+		userAnswers = [];
+		document.querySelectorAll(".feedback").forEach((feedback) => {
+			feedback.innerHTML = "";
+			feedback.classList.remove("correct", "incorrect");
+		});
 
-    let score = 0;
+		let score = 0;
 
-    // Loop through all questions
-    for (let i = 0; i < questionsToDisplay.length; i++) {
-      const selectedAnswer = getSelectedAnswer(`answer${i}`);
-      userAnswers.push(selectedAnswer);
+		// Loop through all questions
+		for (let i = 0; i < questionsToDisplay.length; i++)
+		{
+			const selectedAnswer = getSelectedAnswer(`answer${i}`);
+			userAnswers.push(selectedAnswer);
 
-      currentQuestion = questionsToDisplay[i];
-      //const currentQuestion = questions[i];
+			currentQuestion = questionsToDisplay[i];
 
-      // Verifica se almeno un elemento radio è selezionato
-      const selectedRadio = document.querySelector(
-        `input[name="answer${i}"]:checked`
-      );
+			// check that at least one answer is selected
+			const selectedRadio = document.querySelector(
+				`input[name="answer${i}"]:checked`
+			);
 
-      if (selectedRadio) {
-        const correctAnswerIndex = currentQuestion.correctAnswer;
+			const feedbackElement = document
+			.getElementById(`question${i}`)
+			.querySelector(".feedback");
 
-        const feedbackElement = document
-          .getElementById(`question${i}`)
-          .querySelector(".feedback");
+			if (selectedRadio)
+			{
+				const correctAnswerIndex = currentQuestion.correctAnswer;
 
-        // Converti selectedAnswer in stringa per confronto
-        //const selectedAnswerString = selectedAnswer !== null ? selectedAnswer.toString() : null;
+				// Converti selectedAnswer in stringa per confronto
+				//const selectedAnswerString = selectedAnswer !== null ? selectedAnswer.toString() : null;
 
-        if (parseInt(selectedAnswer) === parseInt(correctAnswerIndex)) {
-          feedbackElement.innerHTML = "Correct!";
-          feedbackElement.classList.add("correct");
-          score += 1;
-        } else {
-          feedbackElement.innerHTML = `Incorrect. Correct answer: ${currentQuestion.options[correctAnswerIndex]}`;
-          feedbackElement.classList.add("incorrect");
-        }
-      } else {
-        // Nessuna risposta selezionata, considera la domanda come sbagliata
-        const feedbackElement = document
-          .getElementById(`question${i}`)
-          .querySelector(".feedback");
-        feedbackElement.innerHTML = "Incorrect. No answer selected.";
-        feedbackElement.classList.add("incorrect");
-      }
-    }
+				// if the answer is correct
+				if (parseInt(selectedAnswer) === parseInt(correctAnswerIndex))
+				{
+					feedbackElement.innerHTML = "Correct!";
+					feedbackElement.classList.add("correct");
+					score += 1;
+				}
+				else
+				{
+					feedbackElement.innerHTML = `Incorrect. Correct answer: ${currentQuestion.options[correctAnswerIndex]}`;
+					feedbackElement.classList.add("incorrect");
+				}
+			}
+			else
+			{
+				// No answer was selected, consider it wrong
+				feedbackElement.innerHTML = "Incorrect. No answer selected.";
+				feedbackElement.classList.add("incorrect");
+			}
+		}
 
-    // Mostra lo score nella pagina
-    const scoreContainer = document.getElementById("score-container");
-    scoreContainer.innerHTML = `Your Score: ${score} / ${questionsToDisplay.length}`;
-    console.log("Score:", score);
-  }
+		// Show final score on the bottom of the page
+		const scoreContainer = document.getElementById("score-container");
+		scoreContainer.innerHTML = `Your Score: ${score} / ${questionsToDisplay.length}`;
+		console.log("Score:", score);
+	}
 
-  function showAllQuestions() {
-    // Svuota il contenitore delle domande prima di visualizzare tutte le domande
-    questionContainer.innerHTML = "";
+	function showAllQuestions()
+	{
+		// empty question container
+		questionContainer.innerHTML = "";
 
-    if (showAllButton.textContent !== "Show all questions and answers") {
-      console.log("showAllQuestions: Show all questions and answers");
-      showAllButton.textContent = "Show all questions and answers";
-      showAllButton.style.backgroundColor = "";
-      showQuestions();
-    } else {
-      console.log("showAllQuestions: Back to quiz");
-      showAllButton.textContent = "Back to quiz";
-      showAllButton.style.backgroundColor = "orange";
-      showQuestions(questions, true);
-    }
-  }
+		if (showAllButton.textContent !== "Show all questions and answers")
+		{
+			console.log("showAllQuestions: Show all questions and answers");
+			showAllButton.textContent = "Show all questions and answers";
+			showAllButton.style.backgroundColor = "";
+			showQuestions();
+		}
+		else
+		{
+			console.log("showAllQuestions: Back to quiz");
+			showAllButton.textContent = "Back to quiz";
+			showAllButton.style.backgroundColor = "orange";
+			showQuestions(questions, true); 
+		}
+	}
 
-  // Function to get the selected answer for a question
-  function getSelectedAnswer(name) {
-    const selectedOption = document.querySelector(
-      `input[name="${name}"]:checked`
-    );
-    return selectedOption ? selectedOption.value : null;
-  }
+	// Function to get the selected answer for a question
+	function getSelectedAnswer(name)
+	{
+		const selectedOption = document.querySelector(
+			`input[name="${name}"]:checked`
+		);
+		return selectedOption ? selectedOption.value : null;
+	}
 });
