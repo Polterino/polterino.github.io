fae5072d6291874d21a940ce105223e3a525e4bb
Michele Gusella <mgusella2@icloud.com>
2024-06-06 18:06:16 +0200

    added top and bottom scroll button

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+function scrollButton()
+{
+	const scrollTopButton = document.getElementById("scrollTopButton");
+    const scrollBottomButton = document.getElementById("scrollBottomButton");
+
+    // Show the button when scrolling down 100px from the top of the document
+    window.addEventListener("scroll", function () {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            scrollTopButton.style.display = "block";
+            scrollBottomButton.style.display = "block";
+        } else {
+            scrollTopButton.style.display = "none";
+            scrollBottomButton.style.display = "none";
+        }
+    });
+
+    // Scroll to the top when the button is clicked
+    scrollTopButton.addEventListener("click", function () 
+    {
+    	window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Smooth scrolling
+        });
+    });
+
+    scrollBottomButton.addEventListener("click", function () {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth' // Smooth scrolling
+        });
+    });
+}
+
+
 document.addEventListener("DOMContentLoaded", function ()
 {
 	const questionContainer = document.getElementById("question-container");
@@ -34,6 +68,9 @@ document.addEventListener("DOMContentLoaded", function ()
 	// Event listener for the Calculate Score button and show all questions
 	calculateButton.addEventListener("click", calculateScore);
 	showAllButton.addEventListener("click", showAllQuestions);
+
+	// enable scroll button
+	scrollButton();
 
 	// function to shuffle the questions and for each question, the answers
 	function shuffleArray(array)
