8c053e23c650da69d4fc6c1a7f016df47f6a2360
Michele Gusella <mgusella2@icloud.com>
2024-06-26 20:53:24 +0200

    changed ccpp questions

--- files/CCPP/Cyber_Migliardi/First_exam_2023/loop3-onsite.c
+++ files/CCPP/Cyber_Migliardi/First_exam_2023/loop3-onsite.c
@@ -7,35 +7,35 @@ int k = 0;
 
 void loop()
 {
-        unsigned int buf[SIZE];
-        unsigned int i = 0;
-        printf("%d Now loop starts...\n", k++);
+    unsigned int buf[SIZE];
+    unsigned int i = 0;
+    printf("%d Now loop starts...\n", k++);
 
-        printf("buf is at %X\n", (unsigned int)buf);
-        for(i=SIZE + 0;i<SIZE + 10;i++)
-        {
-                printf("[%d] = %X\n", i, buf[i]);
-        }
+    printf("buf is at %X\n", (unsigned int)buf);
+    for(i=SIZE + 0;i<SIZE + 10;i++)
+    {
+        printf("[%d] = %X\n", i, buf[i]);
+    }
 
-        *((int*)(&(buf[SIZE + 5]))) = &loop;
-        printf("Now loop returns...\n");
+    *((int*)(&(buf[SIZE + 5]))) = &loop;
+    printf("Now loop returns...\n");
 }
 
 int main(int argc, char** argv)
 {
-        int val = 7;
-        int k = 0;
-        int dim = atoi(argv[1]);
-        unsigned char theBuf[dim];
-        if(argc > 2)
-        {
-                val = atoi(argv[2]);
-        }
-        for(k=0;k<dim;k++)
-        {
-                theBuf[k] = val;
-        }
-        printf("main = %X\n", (unsigned int)main);
-        printf("loop = %X\n", (unsigned int)loop);
-        loop(dim, theBuf);
+    int val = 7;
+    int k = 0;
+    int dim = atoi(argv[1]);
+    unsigned char theBuf[dim];
+    if(argc > 2)
+    {
+        val = atoi(argv[2]);
+    }
+    for(k=0;k<dim;k++)
+    {
+        theBuf[k] = val;
+    }
+    printf("main = %X\n", (unsigned int)main);
+    printf("loop = %X\n", (unsigned int)loop);
+    loop(dim, theBuf);
 }
