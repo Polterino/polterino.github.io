f5a24236a2a618e0b641c6dc16c7c78e9acebe9c
Michele Gusella <michele.gusella@studenti.unipd.it>
2025-02-24 13:00:00 +0100

    spaces -> tabs

--- js/utils.js
+++ js/utils.js
@@ -1,50 +1,50 @@
 const pageTitles = {
-    'digital': 'Digital Forensic and Biometrics',
-    'cogcomp': 'Cognition and Computation',
-    'machine': 'Machine Learning',
-    'cyber': 'Cybersecurity',
-    'webapp': 'Web Applications',
-    'law': 'Law and Data',
-    'hci': 'Human Computer interaction',
-    'infosys': 'Fundamentals of information systems',
-    'ethical': 'Ethical Hacking',
-    'deep': 'Deep learning',
-    'cyberphysicaliot': 'Cyber physical systems and IOT security',
-    'mobile': 'Mobile Security'
+	'digital': 'Digital Forensic and Biometrics',
+	'cogcomp': 'Cognition and Computation',
+	'machine': 'Machine Learning',
+	'cyber': 'Cybersecurity',
+	'webapp': 'Web Applications',
+	'law': 'Law and Data',
+	'hci': 'Human Computer interaction',
+	'infosys': 'Fundamentals of information systems',
+	'ethical': 'Ethical Hacking',
+	'deep': 'Deep learning',
+	'cyberphysicaliot': 'Cyber physical systems and IOT security',
+	'mobile': 'Mobile Security'
 };
 
 const faculties = {
-    cybersex: {
-        thirdSemester: ['ethical', 'cyberphysicaliot', 'mobile'],
-        secondSemester: ['webapp', 'hci', 'deep'],
-        firstSemester: ['cogcomp', 'digital', 'machine', 'cyber', 'law']
-    },
-    compfin: {
-        thirdSemester: ['law'],
-        firstSemester: ['infosys']
-    }
+	cybersex: {
+		thirdSemester: ['ethical', 'cyberphysicaliot', 'mobile'],
+		secondSemester: ['webapp', 'hci', 'deep'],
+		firstSemester: ['cogcomp', 'digital', 'machine', 'cyber', 'law']
+	},
+	compfin: {
+		thirdSemester: ['law'],
+		firstSemester: ['infosys']
+	}
 };
 
 function setCookie(name, value, days) 
 {
-    let expires = "";
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+	let expires = "";
+	if (days) {
+		const date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
 function getCookie(name) 
 {
-    const cookieString = document.cookie;
-    const cookies = cookieString.split('; ');
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].split('=');
-        if (cookie[0] === name) {
-            return cookie[1];
-        }
-    }
-    return null;
+	const cookieString = document.cookie;
+	const cookies = cookieString.split('; ');
+	for (let i = 0; i < cookies.length; i++) {
+		const cookie = cookies[i].split('=');
+		if (cookie[0] === name) {
+			return cookie[1];
+		}
+	}
+	return null;
 }
